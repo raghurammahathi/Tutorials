c1e0afcd1f28b22609cc599133d11911cb0c3d41
SunarcMac <sunarcmac@SunarcMacs-Mac-mini.local>
2018-12-24 16:05:32 +0530

    add custom button in food panda screen

---
+++ DesignDirectoryScreen/DesignDirectoryScreen/View/DesignableButton.swift
@@ -0,0 +1,20 @@
+//
+//  DesignableButton.swift
+//  DesignDirectoryScreen
+//
+//  Created by SunarcMac on 24/12/18.
+//  Copyright © 2018 SunarcMac. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable class DesignableButton: UIButton {
+ 
+    @IBInspectable var fillColor: UIColor = UIColor(red: 16/255.0, green: 206/255.0, blue: 132/255.0, alpha: 1.0)
+    
+    override func draw(_ rect: CGRect) {
+        let path = UIBezierPath(roundedRect: rect, cornerRadius: 10)
+        fillColor.setFill()
+                path.fill()
+    }
+}
