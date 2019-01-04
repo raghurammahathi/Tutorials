b8d4f4326e9daa69380026ac4a8b987ee24656ed
raghuram.mahathi <raghuram.mahathi@sunarctechnologies.com>
2019-01-04 17:52:07 +0530

    changes in custom notifications folder

--- custom notifications(custom UI)/CustomNotifications/CustomNotifications/AppDelegates/AppDelegate.swift
+++ custom notifications(custom UI)/CustomNotifications/CustomNotifications/AppDelegates/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        UNUserNotificationCenter.current().requestAuthorization(options: UNAuthorizationOptions.alert) { (authorized:Bool, error:Error?) in
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert, .sound]) { (authorized:Bool, error:Error?) in
             if !authorized {
                 print("not authorized")
             }
