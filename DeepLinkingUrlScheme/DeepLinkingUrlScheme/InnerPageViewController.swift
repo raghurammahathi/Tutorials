a54394a79afd63e68fc7ad9b2278ea29a01ca7a4
raghuram.mahathi <raghuram.mahathi@sunarctechnologies.com>
2019-01-09 12:02:38 +0530

    add DeepLinkingUrlScheme folder

---
+++ DeepLinkingUrlScheme/DeepLinkingUrlScheme/InnerPageViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InnerPageViewController.swift
+//  DeepLinkingUrlScheme
+//
+//  Created by SunarcMac on 09/01/19.
+//  Copyright © 2019 SunarcMac. All rights reserved.
+//
+
+import UIKit
+
+class InnerPageViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
