5c29b868813d85fb5bd3cee200f6ca7576ad1029
raghuram.mahathi <raghuram.mahathi@sunarctechnologies.com>
2019-01-10 14:39:19 +0530

    code cleanup

--- viewPagerIos/viewPagerIos/ViewController.swift
+++ viewPagerIos/viewPagerIos/ViewController.swift
@@ -14,42 +14,36 @@ class ViewController: UIViewController {
   let cellId = "cellId"
     
     @IBOutlet weak var collection: UICollectionView!
-    
     @IBOutlet weak var tabBarCollection: UICollectionView!
-    
     @IBOutlet weak var barLeftConstraint: NSLayoutConstraint!
-    
     @IBOutlet weak var barView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+         setUpCollection()
+         setUpTabBarCollection()
+    }
+
+    func setUpCollection (){
         collection.backgroundColor = UIColor.blue
         collection.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
         collection.isPagingEnabled = true
-        // Do any additional setup after loading the view, typically from a nib.
-      
+    }
+    func setUpTabBarCollection(){
         tabBarCollection.backgroundColor = UIColor.init(red: 63/255, green: 79/255, blue: 181/255, alpha: 1)
         let selectedIndex = NSIndexPath(item: 0, section: 0)
         tabBarCollection.selectItem(at: selectedIndex as IndexPath, animated: false, scrollPosition: UICollectionView.ScrollPosition.right)
-        
     }
-
-    
 
 }
 extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
     
-    
-    
-    
     func scrollViewDidScroll(_ scrollView: UIScrollView) {
         print(scrollView.contentOffset.x)
         barLeftConstraint.constant = scrollView.contentOffset.x/3
-        
     }
     
-    
     func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
-        
         let index = targetContentOffset.pointee.x/view.frame.width
         let indexPath = NSIndexPath(item: Int(index), section: 0)
         tabBarCollection.selectItem(at: indexPath as IndexPath, animated: false, scrollPosition: UICollectionView.ScrollPosition.right)
@@ -64,32 +58,23 @@ extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource, U
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let colors: [UIColor] = [.gray, .orange, .purple]
-
         if collectionView == self.collection {
             let cell = collection.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
             cell.backgroundColor = colors[indexPath.item]
             return cell
         }
-            
         else {
-            
             if let IconsCell: IconsCell = collectionView.dequeueReusableCell(withReuseIdentifier: "IconsCell", for: indexPath) as? IconsCell
             {
-
                 IconsCell.imgSortItems.image = UIImage(named: sortImages[indexPath.row])?.withRenderingMode(UIImage.RenderingMode.alwaysTemplate)
                 IconsCell.tintColor = UIColor.init(red: 91/255, green: 14/255, blue: 13/255, alpha: 1)
-
                 return IconsCell
             }
             return UICollectionViewCell()
         }
-        
-        
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-       
         if collectionView == self.collection {
             return CGSize(width: view.frame.width, height: view.frame.height)
         }else if collectionView == tabBarCollection{
@@ -100,11 +85,9 @@ extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource, U
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if collectionView == tabBarCollection{
-            
             print(indexPath.item)
             let x = CGFloat(indexPath.item) * view.frame.width/3
             barLeftConstraint.constant = x
-            
 //            UIView.animate(withDuration: 0.75) {
 //                self.view.layoutIfNeeded()
 //            }
@@ -114,32 +97,18 @@ extension ViewController: UICollectionViewDelegate,UICollectionViewDataSource, U
 //            }, completion: nil)
             
            scrolltoMenuIndex(menuIndex: indexPath.item)
-            
         }
-        
     }
-//    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-////        if collectionView == tabBarCollection{
-////        return 0
-////        } else{
-////            return
-////        }
-//        return 0
-//    }
     
     func scrolltoMenuIndex(menuIndex: Int){
     
         let index = NSIndexPath(item: menuIndex, section: 0)
         collection.scrollToItem(at: index as IndexPath, at: UICollectionView.ScrollPosition.left, animated: false)
-    
     }
-    
 }
 
 class IconsCell: UICollectionViewCell {
-    
     @IBOutlet weak var imgSortItems: UIImageView!
-    
     override var isSelected: Bool {
         didSet{
             if isSelected == true {
@@ -147,10 +116,7 @@ class IconsCell: UICollectionViewCell {
             } else {
                 imgSortItems.tintColor = UIColor.init(red: 91/255, green: 14/255, blue: 13/255, alpha: 1)
             }
-            
 //            imgSortItems.tintColor = isHighlighted == false ? UIColor.init(red: 91/255, green: 14/255, blue: 13/255, alpha: 1) : UIColor.white
         }
     }
-    
-    
 }
