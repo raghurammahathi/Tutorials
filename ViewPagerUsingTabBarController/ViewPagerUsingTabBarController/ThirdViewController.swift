10d3cd881f92066c61b9e02e07da0f8699f2deb4
Raghu ram mahathi <raghurammahathi@gmail.com>
2019-01-24 21:53:36 +0530

    add ViewPagerUsingTabBarController folder

---
+++ ViewPagerUsingTabBarController/ViewPagerUsingTabBarController/ThirdViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ThirdViewController.swift
+//  ViewPagerUsingTabBarController
+//
+//  Created by Raghu ram mahathi on 24/01/19.
+//  Copyright © 2019 Raghu ram mahathi. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    @objc func swipeLeft(sender:UISwipeGestureRecognizer) {
+        let total = self.tabBarController!.viewControllers!.count - 1
+        tabBarController!.selectedIndex = min(total, tabBarController!.selectedIndex + 1)
+        
+    }
+    
+    @objc func swipeRight(sender:UISwipeGestureRecognizer) {
+        tabBarController!.selectedIndex = max(0, tabBarController!.selectedIndex - 1)
+    }
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let left = UISwipeGestureRecognizer(target: self, action: #selector(swipeLeft))
+        left.direction = .left
+        self.view.addGestureRecognizer(left)
+        
+        let right = UISwipeGestureRecognizer(target: self, action: #selector(swipeRight))
+        right.direction = .right
+        self.view.addGestureRecognizer(right)
+        
+    }
+    
+    
+    
+    
+}
