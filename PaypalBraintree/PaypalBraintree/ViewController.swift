4e9d3b6df595e714b247557244821c783613f717
raghuram.mahathi <raghuram.mahathi@sunarctechnologies.com>
2019-02-02 13:55:39 +0530

    code clean up

--- PaypalBraintree/PaypalBraintree/ViewController.swift
+++ PaypalBraintree/PaypalBraintree/ViewController.swift
@@ -10,56 +10,64 @@ import UIKit
 import BraintreeDropIn
 import Braintree
 
-class ViewController: UIViewController, BTViewControllerPresentingDelegate, BTAppSwitchDelegate {
 
-    
-    
-    func paymentDriver(_ driver: Any, requestsPresentationOf viewController: UIViewController) {
-        present(viewController, animated: true, completion: nil)
-    }
-    
-    func paymentDriver(_ driver: Any, requestsDismissalOf viewController: UIViewController) {
-        dismiss(animated: true, completion: nil)
-    }
-    
+var braintreeClient: BTAPIClient?
 
-    // MARK: - BTAppSwitchDelegate
-    
-    
-    // Optional - display and hide loading indicator UI
-    func appSwitcherWillPerformAppSwitch(_ appSwitcher: Any) {
-        showLoadingUI()
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(hideLoadingUI), name: NSNotification.Name.NSExtensionHostDidBecomeActive, object: nil)
-    }
-    
-    func appSwitcherWillProcessPaymentInfo(_ appSwitcher: Any) {
-        hideLoadingUI()
-    }
-    
-    func appSwitcher(_ appSwitcher: Any, didPerformSwitchTo target: BTAppSwitchTarget) {
-        
-    }
-    
-    // MARK: - Private methods
-    
-    func showLoadingUI() {
-        // ...
-    }
-    
-    @objc func hideLoadingUI() {
-        NotificationCenter
-            .default
-            .removeObserver(self, name: NSNotification.Name.NSExtensionHostDidBecomeActive, object: nil)
-        // ...
-    }
+class ViewController: UIViewController {
 
     @IBAction func onBtnTap(_ sender: Any) {
         //startCheckout()
         showDropIn(clientTokenOrTokenizationKey: "sandbox_k3g6xwwg_7hy8bbf6d6gcpc55")
     }
-    var braintreeClient: BTAPIClient?
 
+    func showDropIn(clientTokenOrTokenizationKey: String) {
+        let request =  BTDropInRequest()
+        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
+        { (controller, result, error) in
+            if (error != nil) {
+                print("ERROR")
+                print(error!.localizedDescription)
+            } else if (result?.isCancelled == true) {
+                print("CANCELLED")
+            } else if let nonce = result?.paymentMethod?.nonce {
+                
+                let deviceData = PPDataCollector.collectPayPalDeviceData()
+                print("Send this device data to your server: \(deviceData)")
+                self.postNonceToServer(paymentMethodNonce: nonce, deviceData: deviceData)
+            }
+            controller.dismiss(animated: true, completion: nil)
+        }
+        self.present(dropIn!, animated: true, completion: nil)
+    }
+    
+    
+    func postNonceToServer(paymentMethodNonce: String, deviceData: String) {
+        // Update URL with your server
+        
+        print(deviceData)
+        
+        let paymentURL = URL(string: "http://")!
+        var request = URLRequest(url: paymentURL)
+        request.httpBody = "payment_method_nonce=\(paymentMethodNonce)".data(using: String.Encoding.utf8)
+        request.httpMethod = "POST"
+        
+        URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
+            guard let data = data else {
+                print(error!.localizedDescription)
+                return
+            }
+            
+            guard let result = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any], let success = result?["success"] as? Bool, success == true else {
+                print("Transaction failed. Please try again.")
+                return
+            }
+            
+            print("Successfully charged. Thanks So Much")
+            
+            }.resume()
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -94,55 +102,49 @@ class ViewController: UIViewController, BTViewControllerPresentingDelegate, BTAp
 //        }
 //    }
     
+}
+
+extension ViewController: BTViewControllerPresentingDelegate, BTAppSwitchDelegate {
+
     
-        func postNonceToServer(paymentMethodNonce: String, deviceData: String) {
-            // Update URL with your server
-            
-            print(deviceData)
-            
-            let paymentURL = URL(string: "http://")!
-            var request = URLRequest(url: paymentURL)
-            request.httpBody = "payment_method_nonce=\(paymentMethodNonce)".data(using: String.Encoding.utf8)
-            request.httpMethod = "POST"
+    func paymentDriver(_ driver: Any, requestsPresentationOf viewController: UIViewController) {
+        present(viewController, animated: true, completion: nil)
+    }
     
-            URLSession.shared.dataTask(with: request) { (data, response, error) -> Void in
-                guard let data = data else {
-                    print(error!.localizedDescription)
-                    return
-                }
-    
-                guard let result = try? JSONSerialization.jsonObject(with: data, options: []) as? [String: Any], let success = result?["success"] as? Bool, success == true else {
-                    print("Transaction failed. Please try again.")
-                    return
-                }
-    
-                print("Successfully charged. Thanks So Much")
-    
-                }.resume()
-        }
-    
-    
-    
-    func showDropIn(clientTokenOrTokenizationKey: String) {
-        let request =  BTDropInRequest()
-        let dropIn = BTDropInController(authorization: clientTokenOrTokenizationKey, request: request)
-        { (controller, result, error) in
-            if (error != nil) {
-                print("ERROR")
-                print(error!.localizedDescription)
-            } else if (result?.isCancelled == true) {
-                print("CANCELLED")
-            } else if let nonce = result?.paymentMethod?.nonce {
-                
-                let deviceData = PPDataCollector.collectPayPalDeviceData()
-                print("Send this device data to your server: \(deviceData)")
-                self.postNonceToServer(paymentMethodNonce: nonce, deviceData: deviceData)
-            }
-            controller.dismiss(animated: true, completion: nil)
-        }
-        self.present(dropIn!, animated: true, completion: nil)
+    func paymentDriver(_ driver: Any, requestsDismissalOf viewController: UIViewController) {
+        dismiss(animated: true, completion: nil)
     }
     
     
+    // MARK: - BTAppSwitchDelegate
+    
+    
+    // Optional - display and hide loading indicator UI
+    func appSwitcherWillPerformAppSwitch(_ appSwitcher: Any) {
+        showLoadingUI()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(hideLoadingUI), name: NSNotification.Name.NSExtensionHostDidBecomeActive, object: nil)
+    }
+    
+    func appSwitcherWillProcessPaymentInfo(_ appSwitcher: Any) {
+        hideLoadingUI()
+    }
+    
+    func appSwitcher(_ appSwitcher: Any, didPerformSwitchTo target: BTAppSwitchTarget) {
+        
+    }
+    
+    // MARK: - Private methods
+    
+    func showLoadingUI() {
+        // ...
+    }
+    
+    @objc func hideLoadingUI() {
+        NotificationCenter
+            .default
+            .removeObserver(self, name: NSNotification.Name.NSExtensionHostDidBecomeActive, object: nil)
+        // ...
+    }
+    
 }
-
